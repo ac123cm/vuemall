4e8b944c5d084ff8cd41a9f87418c869c0f802d2
ac123cm <ac123cm@163.com>
2021-03-23 18:37:32 +0800

    'tarbarandgoods'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,33 +7,116 @@
     <!-- 轮播图 -->
     <home-swiper :banners="banners"></home-swiper>
     <!-- 推荐 -->
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature></feature>
+    <!-- 控制栏 -->
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+    ></tab-control>
+    <!-- 商品展示 -->
+    <goods-list :goods="goods['pop'].list"></goods-list>
+    <ul>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+      <li>111</li>
+      <li>222</li>
+      <li>333</li>
+      <li>444</li>
+      <li>555</li>
+      <li>666</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/common/navbar/NavBar'
-import HomeSwiper from './childComps/HomeSwiper'
-import {getHomeMultidata} from '@/network/home'
+import NavBar from "@/components/common/navbar/NavBar";
+import TabControl from "@/components/content/tabcontrol/TabControl";
+import GoodsList from '@/components/content/goods/GoodsList'
+import GoodsListItem from '@/components/content/goods/GoodsListItem'
+
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import Feature from "./childComps/Feature";
+
+import { getHomeMultidata, getHomeGoods } from "@/network/home";
 
 export default {
   name: "Home",
-  components:{
+  components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView,
+    Feature,
+    TabControl,
+    GoodsList,
+    GoodsListItem
   },
   data() {
     return {
       banners: [],
-      recommends: []
-    }
+      recommends: [],
+      // 数据模型
+      goods: {
+        // 默认无数据
+        'pop': { page: 0, list: [] },
+        'new': { page: 0, list: [] },
+        'sell': { page: 0, list: [] },
+      },
+    };
   },
   created() {
     // 1.请求多个数据
-    getHomeMultidata().then(res => {
-      // console.log(res);
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
+    this.getHomeMultidata();
+    // 2.请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        // console.log(res)
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then((res) => {
+        // console.log(res)
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
+    },
   },
 };
 </script>
@@ -42,5 +125,14 @@ export default {
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: sticky;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 1000;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
